1edc7d5c223e2c79350fc50f4201883f11be8f86
rafik <siddiquerafik786@gmail.com>
2021-08-12 11:04:33 -1200

    'rafiq'

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/pages/home_page.dart';
+import 'package:flutter_application/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+//import 'package:flutter_application/main.dart';
+//import 'package:flutter_application/pubspec.yml';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        
+      ),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/login",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
+  }
+}
